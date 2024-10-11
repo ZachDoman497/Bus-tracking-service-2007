9b07d62fe651d8b83c2c32afd2ec791aeb647eef
ZachDoman497 <zachary.doman@my.uwi.edu>
2024-10-10 23:11:10 -0400

    Added in a test cpp file with a greeting

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "+---------------------------------------------------+" << endl;
+    cout << "|          Welcome to our 2007 group project!       |" << endl;
+    cout << "| I am currently testing the functionality of github|" << endl;
+    cout << "+---------------------------------------------------+" << endl;
+    return 0;
+}
