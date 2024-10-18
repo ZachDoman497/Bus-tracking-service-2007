be59733e111433592752943cf4a69484b106e326
ZachDoman497 <zachary.doman@my.uwi.edu>
2024-10-17 21:10:46 -0400

    Bus_tracking folder containing main flutter file

---
+++ Bus_tracking/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Uwi Bus Tracker',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex){
+      case 0:
+      page = Placeholder();
+      break;
+      case 1:
+      page = Placeholder();
+      case 2:
+      page = Placeholder();
+      case 3:
+      page = Placeholder();
+      break;
+      default:
+       throw UnimplementedError('no widget for $selectedIndex');
+    }
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.add_business),
+                      label: Text('Bus Tickets'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.report),
+                      label: Text('Report'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.help),
+                     label: Text('Help'),
+                     ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState((){
+                      selectedIndex = value;
+                    });
+        
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+
