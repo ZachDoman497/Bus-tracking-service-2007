93905fd7dcceca406124b048817be91eaa42cda7
ZachDoman497 <zachary.doman@my.uwi.edu>
2024-10-19 16:51:15 -0400

    Added marker functionality and the ability to access the user's location

--- Bus_tracking/lib/pages/map_pages.dart
+++ Bus_tracking/lib/pages/map_pages.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
-  State<MapPage> createState() => _MapPageState();
+  State<MapPage> createState() => MapPageState();
 }
 
-class _MapPageState extends State<MapPage> {
-static const LatLng pTrinidad = LatLng(10.4203732,-61.4654937);
+class MapPageState extends State<MapPage> {
+Location locationController = Location();
+
+static const LatLng _pTrinidad = LatLng(10.4203732,-61.4654937);
+LatLng? currentP;
+
+@override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(initialCameraPosition: CameraPosition(target: pTrinidad, zoom: 11))
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _pTrinidad,
+          zoom: 11),
+          markers: {
+            Marker(markerId: MarkerId("test"), position: _pTrinidad)
+          }
+          ),
       );
+  }
+  Future<void> getLocationUpdates() async{
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await locationController.serviceEnabled();
+    if(serviceEnabled){
+      serviceEnabled = await locationController.requestService();
+    }
+    else{
+      return;
+    }
+
+    permissionGranted = await locationController.hasPermission();
+    if(permissionGranted == PermissionStatus.denied){
+      permissionGranted = await locationController.requestPermission();
+      if(permissionGranted != PermissionStatus.granted){
+        return;
+      }
+    }
+
+    locationController.onLocationChanged.listen((LocationData currentLocation) {
+      if(currentLocation.latitude != null && currentLocation.longitude != null){
+        setState(() {
+          currentP = LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          print(currentP);
+        });
+      }
+    },);
   }
 }
