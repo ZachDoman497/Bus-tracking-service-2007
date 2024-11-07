44aa54f61759ac673375be7968a1169c10b76b1e
RicardoGaitan <ricardo.gaitan@my.uwi.edu>
2024-11-07 19:01:53 -0400

    Removed API KEY

--- Bus_tracking/lib/pages/directions_repository.dart
+++ Bus_tracking/lib/pages/directions_repository.dart
@@ -1,1 +1,34 @@
-// TODO Implement this library.
+import 'package:dio/dio.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'directions_model.dart';
+
+class DirectionsRepository {
+  static const String _baseUrl =
+      'https://maps.googleapis.com/maps/api/directions/json?'; // Correct URL
+
+  final Dio _dio;
+
+  DirectionsRepository({Dio? dio}) : _dio = dio ?? Dio();
+
+  Future<Directions?> getDirections({
+    required LatLng UWI,
+    required LatLng SanFernando,
+  }) async {
+    final response = await _dio.get(
+      _baseUrl,
+      queryParameters: {
+        'origin': '${SanFernando.latitude},${SanFernando.longitude}',
+        'destination': '${UWI.latitude},${UWI.longitude}',
+        'key':
+            'AIzaSyCmq7UJeNpIOzauEIiIcrf49XflF-HOPAc', // Make sure to include your API key
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return Directions.fromMap(response
+          .data); // Assuming you have a Directions model to parse the response
+    } else {
+      return null;
+    }
+  }
+}
